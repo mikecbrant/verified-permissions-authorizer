f15ad4dd88db3c3eb039f6ade301f38bc67b6d83
mikecbrant <2119035+mikecbrant@users.noreply.github.com>
2025-09-06 14:51:02 +0000

    [Pulumi] automated commit for new project vp-authorizer
    Created with app.pulumi.com

---
+++ infra/index.ts
@@ -0,0 +1,7 @@
+// This is an empty Pulumi program in TypeScript.
+// You can add AWS resources here as needed.
+
+import * as pulumi from "@pulumi/pulumi";
+
+// No resources are defined yet.
+// Ready for you to add AWS Verified Permissions resources or any other AWS resources.

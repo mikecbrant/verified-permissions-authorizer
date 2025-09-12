a336d86ae8c5470e9b89b79b90e0df9f7c39bfc7
CharlieHelps <charlie@charlielabs.ai>
2025-09-12 01:45:12 +0000

    refactor(repo): remove legacy packages/provider tree (superseded by providers/{common,pulumi,terraform})

--- packages/provider/pkg/awssdk/dynamo/utils.go
+++
@@ -1,9 +0,0 @@
-package dynamo
-
-import "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-
-// Item is a shorthand for a DynamoDB item.
-type Item = map[string]types.AttributeValue
-
-// StringAttribute renders a string AttributeValue.
-func StringAttribute(s string) types.AttributeValue { return &types.AttributeValueMemberS{Value: s} }
